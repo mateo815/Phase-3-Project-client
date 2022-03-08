07c53a6fb6973f78ab0a87dfa66077ca29e9dd9d
Matt Quinn <mjq0533@gmail.com>
2022-03-07 20:06:03 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from "react";
+
 import './App.css';
 
+
+
 function App() {
+  const [entries, setEntries] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:9292/entries")
+      .then((r) => r.json())
+      .then(setEntries);
+  }, []);
+
+  console.log(entries)
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Hello from React</p>
       </header>
     </div>
   );
