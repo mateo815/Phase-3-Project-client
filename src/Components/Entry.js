baa748737e32f3ae3ea86d2ac7f2158c9d7c7af3
Daniel Zhang <dzhangwh@gmail.com>
2022-03-08 10:58:10 -0500

    more compnents

--- src/Components/Entry.js
+++ src/Components/Entry.js
@@ -2,12 +2,23 @@ import React, { useState } from "react";
 
 function Entry({ e }) {
 
+    const [feel, setFeel] = useState(true)
 
+    function handleFeel() {
+        setFeel({ feel }=> !feel)
+    }
 
     return (
-
+        <div>
+            <h2>e.title</h2>
+            <p>{p.content}</p>
+            <p>
+                <strong> - Fill in </strong>
+            </p>
+            <button onClick={handleFeel}> {feel ? "Show" : "Hide"}
+            </button>
+        </div>
     )
-
 }
 
 export default Entry;
--- src/Components/EntryContainer.js
+++ src/Components/EntryContainer.js
@@ -2,11 +2,19 @@ import React from "react";
 import Entry from "./Entry";
 
 
-function EntryContainer() {
+function EntryContainer({ entry }) {
 
+    const entryList = entry.map((e) => (
+        <Entry
+            key={e.id}
+            e={e} />
+    ))
 
 
     return (
+        <div className="entry-container">
+            {entryList}
+        </div>
 
     );
 }
